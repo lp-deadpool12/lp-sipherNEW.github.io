244028514453419a42d017f9ac822421675fe118
aMadAlien <irasibil2@gmail.com>
2022-04-29 22:42:57 +0300

    add language search

---
+++ js/codeEditor.js
@@ -0,0 +1,6 @@
+const headerSearch = document.querySelector('.header__search-lang');
+if (headerSearch) {
+   headerSearch.addEventListener("click", function (e) {
+      headerSearch.classList.toggle('active');
+   });
+}
