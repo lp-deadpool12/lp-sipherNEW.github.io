e9fbf5b01dde300cf570d4022504be1cdee0db57
Roman <404ramram404@gmail.com>
2022-04-30 17:44:00 +0300

    Js debug hidden

--- js/codeEditor.js
+++ js/codeEditor.js
@@ -9,10 +9,9 @@ document.addEventListener('click', (e)=>{
    const headerLangSearchMenu = document.querySelector('.header__search-lang');
    const headerLangSearchBtn = document.querySelector('.search-lang__btn')
 
-   console.log(clicedElem);
 
    if (clicedElem == openSettingsMenuBtn || clicedElem.parentNode == openSettingsMenuBtn) {
-      console.log(clicedElem);
+
       settingsMenu.classList.add('settings-menu-active');
    }else if(clicedElem.classList.contains('settings-menu__close-cross')){
       settingsMenu.classList.remove('settings-menu-active');
@@ -21,7 +20,7 @@ document.addEventListener('click', (e)=>{
 
 
    if (clicedElem == headerLangSearchBtn || clicedElem == headerLangSearchMenu) {
-      console.log(headerLangSearchMenu);
+
       headerLangSearchMenu.classList.add('active');
    }else if (! e.composedPath().includes(headerLangSearchMenu)) {
       headerLangSearchMenu.classList.remove('active');
