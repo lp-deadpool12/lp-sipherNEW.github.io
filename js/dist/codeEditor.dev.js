82a6a716ae6ae1df0424cdf1626cafa84bc8cf9a
Roman <404ramram404@gmail.com>
2022-04-30 15:59:12 +0300

    delete files

--- js/dist/codeEditor.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var headerSearch = document.querySelector('.header__search-lang');
-
-if (headerSearch) {
-  headerSearch.addEventListener("click", function (e) {
-    headerSearch.classList.toggle('active');
-  });
-}
