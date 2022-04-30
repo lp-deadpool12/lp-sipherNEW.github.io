4d9ba31e34f8f53137ace3e0d3b5c8c6ecabbde8
Roman <404ramram404@gmail.com>
2022-04-30 15:57:07 +0300

    Settings menu open/close

--- js/codeEditor.js
+++ js/codeEditor.js
@@ -1,6 +1,23 @@
+'use strict'
+
 const headerSearch = document.querySelector('.header__search-lang');
 if (headerSearch) {
    headerSearch.addEventListener("click", function (e) {
       headerSearch.classList.toggle('active');
    });
 }
+document.addEventListener('click', (e)=>{
+   let clicedElem = e.target
+   const settingsMenu = document.querySelector('.settings-menu')
+   const openSettingsMenuBtn = document.querySelector(".header__menu")
+
+
+   if (clicedElem == openSettingsMenuBtn || clicedElem.parentNode == openSettingsMenuBtn) {
+      console.log(clicedElem);
+      settingsMenu.classList.add('settings-menu-active');
+   }else if(clicedElem.classList.contains('settings-menu__close-cross')){
+      settingsMenu.classList.remove('settings-menu-active');
+   }
+
+})
+
