1bdd2d3c36ab3f5c08ce8b051363a55f559a13de
glitch-zZz <bogdan.mslv0@gmail.com>
2022-05-05 15:18:38 +0300

    tabs

--- js/codeEditor.js
+++ js/codeEditor.js
@@ -1,59 +1,167 @@
-'use strict'
+/*
+Приблизительная структура табов:
+--tabs
+----tabs__body
+------tabs__items
+--------tabs__item
+----------tab__close-cross
+--------tab__create
+----tabs__content
+------tabs__code
+*/
+function $(el) {
+    return document.querySelector(el)
+}
 
-document.addEventListener('click', (e)=>{
-   let clicedElem = e.target
-   const settingsMenu = document.querySelector('.settings-menu')
-   const openSettingsMenuBtn = document.querySelector(".header__settings-menu")
-   const headerLangSearchMenu = document.querySelector('.header__search-lang');
-   const headerLangSearchBtn = document.querySelector('.header__btn-search-lang')
-   
-   let tabsConunter = 1
-   let getTabs = document.querySelectorAll("div[data-tab]")
-   let getTabsContent = document.querySelectorAll("div[data-tab-content]")
-   const tabsContainer = document.querySelector('.tabs__items') 
-   const tabsContentContainer = document.querySelector('.tabs__content') 
+function $$(el) {
+    return document.querySelectorAll(el)
+}
 
+class Tabs {
+    _tabsBody
+    _tabsContent
+    _tabsItems
+    _tabsItem
+    _tabClose
+    _tabCreate
+    _tabsCode
 
-   function createTab() {
-      let tab = document.createElement('div')
-      tab.classList.add('tabs__item')
-      tab.setAttribute('data-tab', tabsConunter)
-      tab.innerHTML = ` untitled <div class="tab__close-cross">✕</div>`
-      tabsContainer.insertAdjacentElement('beforeend', tab)
-      tabsConunter++
-   }
+    constructor(tabs) {
+        this.tabErrorCheck(tabs)
+        this.takeTabsElements($(`.${tabs}`))
+        this.tabs = tabs;
+        $(`.${tabs}`).addEventListener('click', this.tabsEvents.bind(this))
+        let mutationObserver = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                console.log(mutation);
+            });
+        });
+    }
 
-   function selectTab() {
-      for (let i = 0; i < getTabs.length; i++) {
-         const element = getTabs[i];
-         
-         element.classList.remove('tab-active')
+    // Метод для вызова событий
+    tabsEvents(event) {
+        const target = event.target
+        const isCreateBtn = target.classList.contains('tab__create')
+        const isDeleteBtn = target.classList.contains('tab__close-cross')
+        if (isCreateBtn) {
+            this.createTabs()
+        }
+        if (isDeleteBtn) {
+            this.deleteTabs(event)
+        }
+    }
 
-      }
-   }
+    // Определяет элементы табов внуть свойств
+    takeTabsElements(tabs) {
+        const tabs_body = tabs.querySelector('.tabs__body')
+        const tabs_content = tabs.querySelector('.tabs__content')
+        const tabs_items = tabs.querySelector('.tabs__items')
+        const tabs_item_all = tabs.querySelectorAll('.tabs__item')
+        const tab_close = tabs.querySelector('.tab__close-cross')
+        const tab_create = tabs.querySelector('.tab__create')
+        const tabs_code_all = tabs.querySelectorAll('.tabs__code')
+        if (!tabs_body) {
+            throw new TypeError('Tabs: tabs__body not exist')
+        }
+        if (!tabs_content) {
+            throw new TypeError('Tabs: tabs_content not exist')
+        }
+        if (!tabs_items) {
+            throw new TypeError('Tabs: tabs_items not exist')
+        }
+        if (!tabs_item_all) {
+            throw new TypeError('Tabs: tabs_item not exist')
+        }
+        if (!tab_close) {
+            throw new TypeError('Tabs: tab_close not exist')
+        }
+        if (!tab_create) {
+            throw new TypeError('Tabs: tab_create not exist')
+        }
+        if (!tabs_code_all) {
+            throw new TypeError('Tabs: tabs_code not exist')
+        }
+        this._tabsBody = tabs_body
+        this._tabsContent = tabs_content
+        this._tabsItems = tabs_items
+        this._tabsItem = tabs_item_all
+        this._tabClose = tab_close
+        this._tabCreate = tab_create
+        this._tabsCode = tabs_code_all
 
-   if (clicedElem == openSettingsMenuBtn || clicedElem.parentNode == openSettingsMenuBtn) {
-      settingsMenu.classList.add('settings-menu-active');
-   }else if(clicedElem.classList.contains('settings-menu__close-cross')){
-      settingsMenu.classList.remove('settings-menu-active');
-   }
+    }
 
+    // Метод проверяет на правильность аргумента экземпляра класса для табов. Выражение \\\ const tabs = new Tabs('tabs')
+    tabErrorCheck(potentialError) {
+        let checkElement = $(`.${potentialError}`)
+        if (typeof potentialError !== 'string') {
+            throw new TypeError('Tabs: argument must be a string')
+        }
+        if (!checkElement) {
+            throw new TypeError('Tabs: element not found')
+        }
+        if (potentialError.includes('.')) {
+            throw new TypeError('Tabs: element must be without \'.\' use \'tabs\' instead of \'.tabs\'')
+        }
+    }
 
-   if (clicedElem == headerLangSearchBtn || clicedElem == headerLangSearchMenu) {
+    // Метод для создания нового таба и текстового поля для кода
+    createTabs(event) {
+        const tabs = $(`.${this.tabs}`)
+        // Создание новой вкладки
+        const tabs_item = document.createElement('div')
+        tabs_item.className = 'tabs__item'
 
-      headerLangSearchMenu.classList.add('active');
-   }else if (! e.composedPath().includes(headerLangSearchMenu)) {
-      headerLangSearchMenu.classList.remove('active');
-   }
+        const span = document.createElement('span')
+        span.innerText = 'untitled'
 
-   if (clicedElem.hasAttribute("data-tab-create")) {
-      createTab()
-   }
-   else if (clicedElem.hasAttribute("data-tab")){
-      selectTab()
-      clicedElem.classList.add('tab-active')
-      console.log(clicedElem.previousSibling);
-   }else if(clicedElem.classList.contains('tab__close-cross')){
-      clicedElem.parentNode.remove()
-   }
-})
+        const tabs_close = document.createElement('button')
+        tabs_close.className = 'tab__close-cross'
+        tabs_close.innerText = '✕'
+
+        tabs_item.insertAdjacentElement('afterbegin', span)
+        tabs_item.insertAdjacentElement('beforeend', tabs_close)
+
+        // Помещение новой вкладки ко всем вкладкам
+        this._tabsItems.insertAdjacentElement('beforeend', tabs_item)
+
+        // =====================================================================
+
+        // Создание нового текстового поля для кода
+        const tabs_code = document.createElement('div')
+        tabs_code.className = 'tabs__code'
+        tabs_code.innerText = 'some code'
+
+        // Помещение нового текстового поля для кода
+        this._tabsContent.insertAdjacentElement('beforeend', tabs_code)
+
+        // Изменения табов
+        this._tabsItem = tabs.querySelectorAll('.tabs__item')
+        this._tabsCode = tabs.querySelectorAll('.tabs__code')
+    }
+
+    // Метод для удаление табов
+    deleteTabs(event) {
+        const tabs = $(`.${this.tabs}`)
+        const target = event.target
+        // Определение констант
+        const tabs_item_on_click = target.closest('.tabs__item')
+        const tabs_item_list = [...this._tabsItem]
+        const tabs_code_list = [...this._tabsCode]
+        const index_element = tabs_item_list.findIndex((e) => {
+            return e === tabs_item_on_click
+        })
+        // Удаление элементов
+        if (tabs_item_list.length > 1 || tabs_code_list.length > 1) {
+            tabs_code_list[index_element].remove()
+            tabs_item_list[index_element].remove()
+        }
+
+        // Изменения табов
+        this._tabsItem = tabs.querySelectorAll('.tabs__item')
+        this._tabsCode = tabs.querySelectorAll('.tabs__code')
+    }
+
+}
+
+const tabs = new Tabs('tabs')
