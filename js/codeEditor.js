43bba73a23622b19dd23c0e84f653d393bf88ec8
Roman <404ramram404@gmail.com>
2022-05-05 15:19:33 +0300

    tabs open/close realised

--- js/codeEditor.js
+++ js/codeEditor.js
@@ -6,10 +6,14 @@ const headerLangSearchMenu = document.querySelector('.header__search-lang');
 const headerLangSearchBtn = document.querySelector('.header__btn-search-lang')
 
 let tabsConunter = 1
+
 let getTabs = document.querySelectorAll(".tabs__item")
 let getTabsContent = document.querySelectorAll("tabs__code")
+
 const tabsContainer = document.querySelector('.tabs__items') 
 const tabsContentContainer = document.querySelector('.tabs__content') 
+let currentTab = document.querySelector(".tab-active");
+let currentTabContent = document.querySelector(".tabs__code-active");
 
 function createTab() {
   let tab = document.createElement('div')
@@ -17,17 +21,41 @@ function createTab() {
   tab.setAttribute('data-tab', tabsConunter)
   tab.innerHTML = ` untitled <div class="tab__close-cross">✕</div>`
   tabsContainer.insertAdjacentElement('beforeend', tab)
+  createTabContent()
   tabsConunter++
 }
+function createTabContent() {
+  let tabContent = document.createElement('div')
+  tabContent.setAttribute('data-tab-content', tabsConunter)
+  tabContent.classList.add('tabs__code')
+  
+  tabContent.innerHTML = ` content ${tabsConunter}`
+  tabsContentContainer.insertAdjacentElement('beforeend', tabContent)
 
-function selectTab() { 
-  for (let i = 0; i < getTabs.length; i++) {
-     const element = getTabs[i];
-     
-     element.classList.remove('tab-active')
-
-  }
 }
+function closeTab(tab) {
+   
+}
+
+function selectTab(tab) { 
+   //   for (let i = 0; i < getTabs.length; i++) {
+   //      const element = getTabs[i];
+      
+   //      element.classList.remove('tab-active')
+
+   //   }
+      
+
+   currentTab.classList.remove('tab-active')
+   currentTab = tab
+   currentTab.classList.add('tab-active')
+   let currentTabDataAtrr = currentTab.getAttribute('data-tab')
+   currentTabContent.classList.remove('tabs__code-active')
+   currentTabContent = document.querySelector(`div[data-tab-content = "${currentTabDataAtrr}"]`)
+   currentTabContent.classList.add("tabs__code-active")
+
+}
+
 
 
 document.addEventListener('click', (e)=>{
@@ -51,11 +79,12 @@ document.addEventListener('click', (e)=>{
 
    if (clicedElem.hasAttribute("data-tab-create")) {
       createTab()
+   //   / createTabContent()      
    }
    else if (clicedElem.hasAttribute("data-tab")){
-      selectTab()
-      clicedElem.classList.add('tab-active')
-      console.log(clicedElem.previousSibling);
+      selectTab(clicedElem)
+      
+      //console.log(clicedElem.previousSibling);
    }else if(clicedElem.classList.contains('tab__close-cross')){
       clicedElem.parentNode.remove()
    }
